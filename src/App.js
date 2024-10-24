1145208f998e8031d3d229869c7e772515a845ae
varijaalur <varija17@gmail.com>
2024-10-24 18:32:37 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,35 @@ import AdminRoutes from "./Routers/AdminRoutes";
 import NotFound from "./Pages/Notfound";
 import AdminPannel from "./Admin/AdminPannel";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUser } from "./Redux/Auth/Action";
-// import Routers from './Routers/Routers';
 
 function App() {
-  const {auth}=useSelector(store=>store);
+  const { auth } = useSelector((store) => store);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true); // Add a loading state
   const jwt = localStorage.getItem("jwt");
+
   useEffect(() => {
     if (jwt) {
-      dispatch(getUser(jwt));
+      dispatch(getUser(jwt)).then(() => setLoading(false)); // Set loading to false after user is fetched
+    } else {
+      setLoading(false); // If no jwt, we are not loading
     }
   }, [jwt]);
+
+  if (loading) {
+    return <div>Loading...</div>; // You can show a spinner or any loading screen here
+  }
+
   return (
     <div className="">
       <Routes>
         <Route path="/*" element={<CustomerRoutes />} />
-       {auth.user?.role==="ROLE_ADMIN" && <Route path="/admin/*" element={<AdminPannel />} />}
+        {auth.user?.role === "ROLE_ADMIN" && (
+          <Route path="/admin/*" element={<AdminPannel />} />
+        )}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
